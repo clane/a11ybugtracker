307068f948459579c2ca15d136fe8147bbbb768f
Chris Lane <chlane@vmware.com>
2020-05-06 23:06:51 -0700

    escaping html chars and quotes

--- helpers/vars.php
+++ helpers/vars.php
@@ -7,12 +7,16 @@ $table = "testtable1";
 
 if($_GET["database"]) {  
 	$database = htmlspecialchars($_GET["database"]); 
+} elseif($_POST["database"]) {  
+	$database = htmlspecialchars($_POST["database"]); 
 } else {
 	$database = "a11ybugtracker"; 
 }
 
 if($_GET["table"]) {  
 	$table = htmlspecialchars($_GET["table"]); 
+} elseif($_POST["table"]) {  
+	$table = htmlspecialchars($_POST["table"]); 
 } else {
 	$table = "testtable1"; 
 }
