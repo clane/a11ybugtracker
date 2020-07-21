5bc70dbf3dce19909a9ef88dbf0722ab8b4cc657
Santina Croniser <9728572+scroniser@users.noreply.github.com>
2020-07-21 09:48:58 -0700

    change home link

--- helpers/top.php
+++ helpers/top.php
@@ -11,7 +11,7 @@
 	<body>
     <div id="page" class="main-container">
 	<header class="header-4">
-		<div class="branding"><img class="logo nav-link nav-vertical-center" src="https://www.vmware.com/etc/clientlibs/vmwaredevapp/clientlib-nav-redesign/images/vm-logo.png" alt="vmware" style="height:22px;"> <h1 class="title nav-vertical-center"><a id="home-link" href="index.php?database=$database&table=$table">A11y Reporting</a></h1></div>
+		<div class="branding"><img class="logo nav-link nav-vertical-center" src="https://www.vmware.com/etc/clientlibs/vmwaredevapp/clientlib-nav-redesign/images/vm-logo.png" alt="vmware" style="height:22px;"> <h1 class="title nav-vertical-center"><a id="home-link" href="index.php">A11y Reporting</a></h1></div>
 	</header>
 		<div class="content-container">
 			<main class="content-area">
