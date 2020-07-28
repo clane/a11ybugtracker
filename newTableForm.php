b9086b86243384752481327ec1d135471cda2dff
root <root@cxguidelines.vmware.com>
2020-07-28 18:18:26 -0400

    added some table naming instructions

--- newTableForm.php
+++ newTableForm.php
@@ -6,8 +6,9 @@ include  'helpers/top2.php';
 print "<h1>$pageTitle</h1>";   
 
 $form = "<form action=\"createTable.php\" method=\"post\">";
-$form .= "<label for='newTable'>New Table Name</label>";
-$form .= "<input type=\"text\" id='newTable' name='newTable' />";
+$form .= "<label for='newTable' placeholder='windows_v10_072820'>New Table Name</label>";
+$form .= "<div id='format'>Name the table with the following format - product_version_date </div>";
+$form .= "<input type=\"text\" id='newTable' placeholder='tanzu_v2_010120' name='newTable' aria-describedby='format' />";
 $form .= "<input type='submit'>";
 $form .= "</form>";
 
