34d838efa2fce2572e19719c0d1a6db64a3a98e7
root <root@cxguidelines.vmware.com>
2020-07-28 10:45:55 -0400

    added table creation and form

--- createTable.php
+++ createTable.php
@@ -2,18 +2,18 @@
 
 require 'helpers/connect.php';
 require 'helpers/vars.php';
-include 'helpers/top.php'; 
+include 'helpers/top2.php'; 
 include 'helpers/getColNames.php'; 
 
-print "<h1>Table Creation - $database.$table</h1>"; 
+print "<h1>Table Creation - database:$database</h1>"; 
 
-$newTable =  $_GET['newTable'];
+$newTable =  $_POST['newTable'];
 
 $query =  "CREATE TABLE $database.$newTable LIKE $database.template";
 
 if ($result = mysqli_query($conn, $query)) {
 	echo "<p>$database.$newTable has been created</p>";
-
+        $table = $newTable;
 echo "<p><a href=\"./index.php?database=reporting&table=$newTable\">Go to $database.$newTable home page</a></p>";
 
  } else {
---
+++ newTableForm.php
@@ -0,0 +1,22 @@
+<?php
+require 'helpers/connect.php';
+require 'helpers/vars.php';
+include  'helpers/top.php'; 
+
+print "<h1>Create a New Table</h1>";
+
+$form = "<form action=\"createTable.php\" method=\"post\">";
+$form .= "<label for=''>New Table Name</label>";
+$form .= "<input type=\"text\" id='newTable' name='newTable' />";
+$form .= "<input type='submit'>";
+$form .= "</form>";
+
+echo $form;
+
+
+include  'helpers/bottom.php'; 
+
+?>
+
+
+
